045858c957e4232e63eb7e4f4430114a0cb03979
Aurélien Deharbe <aurelien.deharbe@gmail.com>
2014-04-10 15:42:55 +0200

    added free names computation

--- src/Pi/Syntax.hs
+++ src/Pi/Syntax.hs
@@ -1,5 +1,8 @@
 module Pi.Syntax where
 
+import Data.Set
+
+
 type Name = String
 
 data Action = Tau
@@ -27,6 +30,17 @@ data Script = Script { scriptDefs :: [Def]
                      }
                      deriving (Eq, Show)
 
+freeNames :: Process -> Set.Set Name
+freeNames Nil = Set.empty
+freeNames (Act Tau p) = freeNames p
+freeNames (Act (Input c x) p) = Set.insert c $ Set.delete x $ freeNames p
+freeNames (Act (Output c x) p) = Set.insert c $ Set.insert x $ freeNames p
+freeNames (Local n p) = Set.delete n $ freeNames p
+freeNames (New n p) = Set.delete n $ freeNames p
+freeNames (Par p q) = freeNames p `Set.union` freeNames q
+freeNames (Sum p q) = freeNames p `Set.union` freeNames q
+freeNames (Call _ as) = Set.fromList as
 
-
+unfold :: Reader [Def] Process -> Reader [Def] Process
+unfold = error "unfold not yet implemented"
 
